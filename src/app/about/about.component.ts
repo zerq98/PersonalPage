777465aafaaec88ce348d755d7ba395c4a63123c
Mateusz Trybuła <zerq1998@gmail.com>
2023-01-14 22:39:54 +0100

    Add about me page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  content: string="I'm a web developer located in poland. I mostly focus on backend site of web development but I never close myself to new challenges.";
+  hobby: string="I love spending my free time playing football and computer games with my friends or reading good fantasy books."
 }
